e0f03b03a22c1a610b4ff123decb847df150dd4e
Rick de Saussure <rickde@mac.com>
2017-05-15 12:16:10 -0600

    Created Project First Commit
    Signed-off-by: Rick de Saussure <rickde@mac.com>

---
+++ MemeMe1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe1.0
+//
+//  Created by Rick de Saussure on 5/15/17.
+//  Copyright © 2017 Rick de Saussure. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
