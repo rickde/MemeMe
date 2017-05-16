c33fcf14dfd8d88fc978c467b045f31209823dd8
Rick de Saussure <rickde@mac.com>
2017-05-16 10:48:33 -0600

    Added Image Picker Outlet and Action
    Signed-off-by: Rick de Saussure <rickde@mac.com>

--- MemeMe1.0/ViewController.swift
+++ MemeMe1.0/ViewController.swift
@@ -9,15 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func pickAnImage(_ sender: Any) {
+        
+        let pickerController = UIImagePickerController()
+        present(pickerController, animated: true, completion: nil)
     }
 
 
